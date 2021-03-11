d37c63eb037f639943ee5f2762bb856503b3165b
Wenrui <58852708+wenruiq@users.noreply.github.com>
2021-03-11 09:22:53 +0800

    changed to vsc

--- vim-stuff/algo/max_subarr_sum.cpp
+++ vim-stuff/algo/max_subarr_sum.cpp
@@ -16,8 +16,10 @@ int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+
 	int arr[] = {-1,2,4,-3,5,2,-5,2};
 	int n = sizeof(arr)/sizeof(arr[0]);
+
 	cout<<max_subarr_sum(arr,n)<<"\n";
 
 	return 0;
